4469cf138d12ef1b213e5e88b77092a6751dcb2a
29bucuk <pinargultekin@yahoo.com>
2019-05-18 07:51:31 -0400

    style is created

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//variables
+var score = 0;
+var wins = 0;
+var losses= 0;
+var randomNumber= 0;
+var totalScore= 0;
+var red = 0;
+var blue = 0;
+var yellow = 0;
+var green = 0; 
+
+//functions
+
+//Setting random numbers
