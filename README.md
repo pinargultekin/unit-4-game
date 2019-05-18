7055fcdae8a4bdc57334ddbf3333f1d98cf76098
29bucuk <pinargultekin@yahoo.com>
2019-05-18 16:37:02 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # unit-4-game
+# Crystal Collector
+---------------
+## Table of Contents
+1. [Abstract](#abstract)
+2. [Coding Overview](#overview)
+3. [Game Display](#display)
+
+<a name="abstract"></a>
+## Abstract
+There are 4 different gems and each of them has a random value.
+By clicking them a user collects a specific amount of points to reach the random number.
+Once a player's collection of gem value equals to random number, user wins.
+Otherwise user loses and scores are written on the board and game started again.
+
+<a name="overview"></a>
+## Coding Overview
+
+The game is built with;
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* jQuery
+
+<a name="display"></a>
+## Game Display
+<img src="assets/images/gem.png" alt="game-display">
+<br>
+<img src="assets/images/gem2.png" alt="game-display">
+
+### P.S.
+
+<a href="https://www.freepik.com/free-photos-vectors/background">Vectors created by macrovector - www.freepik.com</a>
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 //variables
 var score = 0;
 var wins = 0;
-var losses= 0;
-var randomNumber= 0;
-var totalScore= 0;
+var losses = 0;
+var randomNumber = 0;
+var totalScore = 0;
 var red = 0;
 var blue = 0;
 var yellow = 0;
-var green = 0; 
+var green = 0;
 
 //functions
 
@@ -15,76 +15,83 @@ var green = 0;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-randomNumber = getRandomInt(19,120);
-console.log("randomNumber is: " , randomNumber);
+randomNumber = getRandomInt(19, 120);
+console.log("randomNumber is: ", randomNumber);
 $("#randomNumber").text(randomNumber);
 
-red = getRandomInt(1,12);
-console.log("red crystal value is: " , red);
+red = getRandomInt(1, 12);
+console.log("red crystal value is: ", red);
 
 
-blue = getRandomInt(1,12);
-console.log("blue crystal value is: " , blue);
+blue = getRandomInt(1, 12);
+console.log("blue crystal value is: ", blue);
 
 
-yellow = getRandomInt(1,12);
-console.log("yellow crystal value is: " , yellow);
+yellow = getRandomInt(1, 12);
+console.log("yellow crystal value is: ", yellow);
 
 
-green = getRandomInt(1,12);
-console.log("green crystal value is: " , green);
+green = getRandomInt(1, 12);
+console.log("green crystal value is: ", green);
 
+$("#totalScore").text(totalScore);
 //dom interaction
 
 //adding value to total score when user click each gem/button
-$("#red").on("click", function () { 
+$("#red").on("click", function () {
     totalScore += red;
-    console.log("total score is : " , totalScore);
+    $("#totalScore").text(totalScore);
+    console.log("total score is : ", totalScore);
     check(); //check the total score if user win or lost
 });
 
-$("#blue").on("click", function () { 
+$("#blue").on("click", function () {
     totalScore += blue;
-    console.log("total score is : " , totalScore);
+    $("#totalScore").text(totalScore);
+    console.log("total score is : ", totalScore);
     check();
 });
 
-$("#yellow").on("click", function () { 
+$("#yellow").on("click", function () {
     totalScore += yellow;
-    console.log("total score is : " , totalScore);
+    $("#totalScore").text(totalScore);
+    console.log("total score is : ", totalScore);
     check();
-    
+
 });
-$("#green").on("click", function () { 
+$("#green").on("click", function () {
     totalScore += green;
-    console.log("total score is : " , totalScore);
+    $("#totalScore").text(totalScore);
+    console.log("total score is : ", totalScore);
     check();
 });
 
 //Final condition check to finish the game
-function check () {
-    if ( totalScore === randomNumber){
+function check() {
+    if (totalScore === randomNumber) {
         alert("You won! Keep up the work!");
         wins++;
         console.log("Wins: " + wins);
         $("#wins").text(wins);
         reset();
-    }else if ( totalScore > randomNumber){
+    } else if (totalScore > randomNumber) {
         alert("You lost! Try harder next time!");
         losses++;
         console.log("Losses: " + losses);
         $("#losses").text(losses);
         reset();
     }
-   }
+}
 
 //reset
-function reset () {
-    totalScore= 0;
-    red = getRandomInt(1,12);
-    blue= getRandomInt(1,12);
-    yellow = getRandomInt(1,12);
-    green= getRandomInt(1,12);
-    randomNumber = getRandomInt(19,120);
-$("#totalScore").text(totalScore);
+function reset() {
+    totalScore = 0;
+    randomNumber = getRandomInt(19, 120);
+    $("#randomNumber").text(randomNumber);
+    red = getRandomInt(1, 12);
+    blue = getRandomInt(1, 12);
+    yellow = getRandomInt(1, 12);
+    green = getRandomInt(1, 12);
+
+    $("#totalScore").text(totalScore);
 }
